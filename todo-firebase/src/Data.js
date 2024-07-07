4d2701c6876dd21b9863265128a64c38c8921e37
Mohammed owais <owaismohammed795@gmail.com>
2024-07-07 23:31:18 +0530

    Create Data.js

---
+++ todo-firebase/src/Data.js
@@ -0,0 +1,4 @@
+export const firebase_url = "https://todo-app-b5bb3-default-rtdb.firebaseio.com"
+export const api_key = "AIzaSyCEAHvyGiVCRfL96DU15eZWklRr8Yg82ZY"
+
+//Isko .gitignore me daalne se pehle commit kardoge to ignore nahi karta h
