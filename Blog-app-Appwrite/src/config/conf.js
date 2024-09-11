aba9116ab09c07e47f1b193059b6d90f1d662ca6
Mohammed Owais <owaismohammed795@gmail.com>
2024-09-11 15:16:31 +0530

    Initialized store, slices and appwrite configuration

---
+++ Blog-app-Appwrite/src/config/conf.js
@@ -0,0 +1,10 @@
+
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_ENDPOINT), //String me convert kar rhe coz sometimes issues hote he at times
+    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default conf
