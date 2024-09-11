37bd2e0f98ca5491c17bf568d4c3c6dc08f72cf7
Mohammed Owais <owaismohammed795@gmail.com>
2024-09-11 19:43:05 +0530

    Made appwrite services (post and auth). Added sample env file

---
+++ Blog-app-Appwrite/src/appwrite (service)/auth.js
@@ -0,0 +1,78 @@
+import conf from '../config/conf.js'
+import { Client, Account, ID } from "appwrite";
+
+// const client = new Client()
+//     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+//     .setProject('<PROJECT_ID>');               // Your project ID
+
+// const account = new Account(client);
+
+// const promise = account.create('[USER_ID]', 'email@example.com', '');
+
+// promise.then(function (response) {
+//     console.log(response);
+// }, function (error) {
+//     console.log(error);
+// });
+
+
+//We could've simply copy pasted the rest of the code from appwrite doc and it would've worked but realize that we would have to create accounts manually(see above) and expose this in our Register component, so this is like a mixture of UI and business logic. Instead a better approach is below
+
+export class AuthService{
+    client = new Client(); //Observe karo ki let const nahi use kia he, class member he islie
+    account;
+
+    constructor(){
+        this.client         //this.client = client nahi kar rhe he C++ ki tarah...
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId); 
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, username, password}) {//similarly isme function keywrd nahi use ho rha
+        //try catch as there might be times when appwrite servers are down/anything
+        try {
+            const userAccount = await this.account.create(ID.unique(),email, password, username);
+            if(userAccount){
+                return this.login(email, password);
+            }else {
+                return  userAccount;
+            }
+        } catch (error) {
+            console.log(`Appwrite Error in account creation: ${error}`)
+        }
+    }
+
+    async login({email, password}){
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            console.log(`Appwrite Error in login: ${error}`)
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log('Appwrite service :: getCurrentUser :: error', error);
+        }
+        return null; //This is if it goes to the catch block but this memfunction should return smthing right/ if else also works
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite error:: Logout", error)
+        }
+    }
+
+}
+
+const authService = new AuthService();
+
+export default authService;//Class pass karne ki bajae ek object hi pass kar dia h taaki directly .func call karlo warna object create karne padte
+
+
+//Now you would be using this class's object wherever you need. You would be simply be calling the mem fns that you would want. And now let's say you want to shift your services to Firebase or some other service provider the only change you would have to do is here in this class. All the other places would still be calling these member functions only.
---
+++ Blog-app-Appwrite/src/appwrite (service)/config.js
@@ -0,0 +1,138 @@
+import conf from '../config/conf.js'
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class DbService{
+    client = new Client();
+    databases;
+    storage;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.storage = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument( //The place where slug isused is simply document ID, could've used ID.unique.
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite error :: createPost :: error", error);
+        }
+    }
+
+    async updatePost( slug, {title, content, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite error :: updatePost :: error", error);           
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite error :: DeletePost :: error", error);
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite error :: getPost :: error", error);
+            return false;             
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){ //Here queries is just a var name and you need to have an index in which a key called status and that's when you'll be able to use them. If we would've used enum then it would've been even better 
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            )
+        } catch (error) {
+            console.log("Appwrite error :: getPosts :: error", error);
+            return false;            
+        }
+    }
+
+    //File services
+    async uploadFile(file){ //This is the actual file itself and not it's title or something
+        try {
+            return await this.storage.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite error :: uploadFile :: error", error)
+            return false
+        }
+
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.storage.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite error :: deleteFile :: error", error);
+            return false
+        }
+    }
+
+    filePreview(fileId){ //this ain't a async fn as this returns the preview as soon as it's called
+        try {
+            return this.storage.getFilePreview(
+                conf.appwriteBucketId,
+                fileId
+            )
+        } catch (error) {
+            console.log("Appwrite error :: getFilePreview :: error", error);
+            return false
+        }
+    }
+
+}
+
+const dbservice = new DbService();
+
+export default dbservice;
